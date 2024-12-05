e0f4fe6897b5ec322039a9b96123f95c0936504b
AndersKarlAxel_Gyllenhoff <ujx4ab@virginia.edu>
2024-12-05 10:29:07 -0500

    generated comment tree 18vuw2v

--- src/LLM/Mistral/minstral_utils.py
+++ src/LLM/Mistral/minstral_utils.py
@@ -34,25 +34,27 @@ def initialize_tokenizer(model_name: str):
     tokenizer.bos_token_id = 1  # Set beginning of sentence token id
     return tokenizer
 
-def zero_shot_json_to_skeleton(input_data):
+def generate_mistral_prompt_zero_shot(
+        post_title, 
+        post_content, 
+        user_comment_history, 
+        previous_comments_on_thread, 
+        comment_of_interest,
+    ):
     """
     Extracts zero_shot_json_to_skeleton json and create prompt to feed into minstral. 
     Returns minstral output in string format.
     """
     
-    # extract relevant portion of input
-    context = input_data['input']['context']
-    comment_id = input_data['input']['comment_id']
-    
     # Construct the Mistral prompt
     mistral_prompt = f"""
     [INST] 
-    ["There is a particular user that we want to predict the response of on a Reddit post. Use the user's post, the post information, the comments in the thread, and the user's comment history to generate english response to the comment of interest. If any of this information is None, respond with what is available:]
-    [The post's title]: {context['post_title']}
-    [The post's content]: {context['post_content']}
-    [Previous user comments on the thread]: {context['previous_comments_on_thread']}
-    [User's comment history]: {context['user_comment_history']}
-    [Comment of interest]: {context['comment_of_interest']}
+    [Prompt]: "There is a particular user that we want to predict the response of on a Reddit post. Use the user's post, the post information, the comments in the thread, and the user's comment history to generate english response to the comment of interest."
+    [The post's title]: {post_title}
+    [The post's content]: {post_content}
+    User's comment history]: {user_comment_history}
+    [Previous user comments on the thread]: {previous_comments_on_thread}
+    [Comment of interest]: {comment_of_interest}
     [/INST]
     """
     return mistral_prompt
